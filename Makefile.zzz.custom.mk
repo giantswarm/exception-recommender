456624c7dbc382cbcb322d3424a7b0d34a693047
Franco <franco@giantswarm.io>
2024-04-11 12:37:33 -0300

    Add envtest to custom Makefile

---
+++ Makefile.zzz.custom.mk
@@ -0,0 +1,15 @@
+##@ Build Dependencies
+ENVTEST_K8S_VERSION = 1.27.1
+GOBIN=$(shell go env GOPATH)/bin
+
+## Location to install dependencies to
+LOCALBIN ?= $(shell pwd)/bin
+$(LOCALBIN):
+	mkdir -p $(LOCALBIN)
+
+ENVTEST ?= $(LOCALBIN)/setup-envtest
+
+.PHONY: test
+test: ## Runs go test with default values.
+	@echo "====> $@"
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path)" go test -ldflags "$(LDFLAGS)" ./...
